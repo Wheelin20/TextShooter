a1e957d5adc3ad07ec99656f0d40398826bd5840
Kevin Emigh <kevin.emigh@ohiohealth.com>
2016-03-01 22:02:33 -0500

    organizes some files and adds the EnemyNode.swift file

---
+++ TextShooter/EnemyNode.swift
@@ -0,0 +1,15 @@
+//
+//  EnemyNode.swift
+//  TextShooter
+//
+//  Created by Kevin Emigh on 3/1/16.
+//  Copyright © 2016 Kevin Emigh. All rights reserved.
+//
+
+import UIKit
+import SpriteKit
+
+class EnemyNode: SKNode
+{
+    
+}
