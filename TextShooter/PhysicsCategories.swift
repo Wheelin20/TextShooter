960d25e91494298cbd85af58c6a0d0e30f69263d
Kevin Emigh <kevin.emigh@ohiohealth.com>
2016-03-01 23:10:11 -0500

    adds physics categories and BulletNode

---
+++ TextShooter/PhysicsCategories.swift
@@ -0,0 +1,13 @@
+//
+//  PhysicsCategories.swift
+//  TextShooter
+//
+//  Created by Kevin Emigh on 3/1/16.
+//  Copyright © 2016 Kevin Emigh. All rights reserved.
+//
+
+import Foundation
+
+let PlayerCategory: UInt32 = 1 << 1
+let EnemyCategory: UInt32 = 1 << 2
+let PlayerMissleCategory: UInt32 = 1 << 3
